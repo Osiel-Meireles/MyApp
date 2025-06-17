cec1a5389b16cc860d5095a223ddd623c684965b
Osiel-Meireles <osieljr2011@hotmail.com>
2025-06-16 22:42:38 -0300

    Fixing bugs

--- Calculos.cs
+++ Calculos.cs
@@ -3,25 +3,25 @@ class Calculos
     public static void Soma(double numeroUm, double numeroDois)
     {
         double operacao = numeroUm + numeroDois;
-        Console.WriteLine(numeroUm + " + " + numeroDois + " = " + operacao);
+        Console.WriteLine($"{numeroUm} + {numeroDois} = {operacao}");
     }
 
     public static void Subtracao(double numeroUm, double numeroDois)
     {
         double operacao = numeroUm - numeroDois;
-        Console.WriteLine(numeroUm + " - " + numeroDois + " = " + operacao);
+        Console.WriteLine($"{numeroUm} - {numeroDois} = {operacao}");
     }
 
     public static void Divisao(double numeroUm, double numeroDois)
     {
         double operacao = numeroUm / numeroDois;
-        Console.WriteLine(numeroUm + " / " + numeroDois + " = " + operacao);
+        Console.WriteLine($"{numeroUm} / {numeroDois} = {operacao}");
     }
 
     public static void Multiplicacao(double numeroUm, double numeroDois)
     {
         double operacao = numeroUm * numeroDois;
-        Console.WriteLine(numeroUm + " * " + numeroDois + " = " + operacao);
+        Console.WriteLine($"{numeroUm} * {numeroDois} = {operacao}");
     }
 
 }
