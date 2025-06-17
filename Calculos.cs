c618a8cc37110172458a871b0032bf29dcc922c2
Osiel-Meireles <osieljr2011@hotmail.com>
2025-06-16 21:35:24 -0300

    Adding files

---
+++ Calculos.cs
@@ -0,0 +1,22 @@
+class Calculos
+{
+    public static double Soma(double numeroUm, double numeroDois)
+    {
+        return numeroUm + numeroDois;
+    }
+
+    public static double Substracao(double numeroUm, double numeroDois)
+    {
+        return numeroUm - numeroDois;
+    }
+
+    public static double Divisao(double numeroUm, double numeroDois)
+    {
+        return numeroUm / numeroDois;
+    }
+
+    public static double Multiplicacao(double numeroUm, double numeroDois)
+    {
+        return numeroUm * numeroDois;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace MyApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite dois números para soma. Digite o primeiro, pressione ENTER e digite o segundo.");
+        double numeroUm = double.Parse(Console.ReadLine());
+        double numeroDois = double.Parse(Console.ReadLine());
+        Console.WriteLine(" ");
+        Console.WriteLine("Escolha qual das operações deseja fazer:");
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("1 - SOMA");
+        Console.WriteLine("2 - SUBTRAÇÃO");
+        Console.WriteLine("3 - DIVISÃO");
+        Console.WriteLine("4 - MULTIPLICAÇÃO");
+        Console.WriteLine("0 - SAIR");
+
+        int numeroDaOpcao = int.Parse(Console.ReadLine());
+
+        while (numeroDaOpcao > 0)
+        {
+            switch (numeroDaOpcao)
+            {
+                case 1:
+                    Console.WriteLine(numeroUm + " + " + numeroDois + " = " + Calculos.Soma(numeroUm, numeroDois));
+                    break;
+                case 2:
+                    Console.WriteLine(numeroUm + " - " + numeroDois + " = " + Calculos.Substracao(numeroUm, numeroDois));
+                    break;
+                case 3:
+                    Console.WriteLine(numeroUm + " / " + numeroDois + " = " + Calculos.Divisao(numeroUm, numeroDois));
+                    break;
+                case 4:
+                    Console.WriteLine(numeroUm + " * " + numeroDois + " = " + Calculos.Multiplicacao(numeroUm, numeroDois));
+                    break;
+            }
+            Console.WriteLine("Digite a opção que deseja.");
+            numeroDaOpcao = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Obrigado por usar a calculadora.");
+    }
+}
